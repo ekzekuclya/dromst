422467cdcf09b139e3e6cd56056c9537d0ac92d4
ekzekuclya <ekzekuciya07@gmail.com>
2024-03-10 14:43:04 +0600

    cors fix 2

--- core/settings.py
+++ core/settings.py
@@ -63,7 +63,7 @@ SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_SAMESITE = None
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_DOMAIN = '.dromstor.com'
-CSRF_TRUSTED_ORIGINS = ['.dromstor.com']
+CSRF_TRUSTED_ORIGINS = ['https://backend.dromstor.com']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
